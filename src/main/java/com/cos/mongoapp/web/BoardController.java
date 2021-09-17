189a38182f59606f6896a25671c682d6bf35ec6c
shin <shin@naver.com>
2021-09-17 17:42:50 +0900

    업데이트& 딜리트

--- src/main/java/com/cos/mongoapp/web/BoardController.java
+++ src/main/java/com/cos/mongoapp/web/BoardController.java
@@ -2,8 +2,11 @@ package com.cos.mongoapp.web;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,27 @@ public class BoardController {
 
 	private final BoardRepository boardRepository;
 	
+	@PutMapping("/board/{id}")
+	public Board update(@RequestBody BoardSaveDto dto, @PathVariable String id) {
+		
+		Board board = dto.toEntity();
+		board.set_id(id);
+		
+		return boardRepository.save(board);
+	}
+	
+	@DeleteMapping("/board/{id}")
+	public int deleteById(@PathVariable String id) {
+		boardRepository.deleteById(id); // 
+		return 1;
+	}
+	
+	
+	@GetMapping("/board/{id}")
+	public Board findById(@PathVariable String id) {
+		return boardRepository.findById(id).get();
+	}
+	
 	@GetMapping("/board")
 	public List<Board> findAll() { // 리턴을 java오브젝트로 하면 스프링 내부적으로 JSON으로 자동변환해준다.
 		return boardRepository.findAll();
@@ -26,6 +50,9 @@ public class BoardController {
 	
 	@PostMapping("/board")
 	public Board save(@RequestBody BoardSaveDto dto) { // {"title":"제목3", "content":"내용3"})
-		return boardRepository.save(dto.toEntity());
+		
+		Board boardEntity = boardRepository.save(dto.toEntity());
+		
+		return boardEntity;  // MessageConverter발동 -> 자바오브젝트를 json 변환해서 응답함.
 	}
 }
